3ce0fa7b7103f876a322bc0e3fc95de5cabb7ac2
Julia Bergman <julia.bergman@helsinki.fi>
2020-09-20 21:27:25 +0300

    first draft

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///bergmjul"
+db  = SQLAlchemy(app)
+
+@app.route("/ingredients")
+def list_ingredients():
+    result = db.session.execute("SELECT id, name FROM ingredient")
+    ingredients = result.fetchall()
+    return render_template("ingredients.html", ingredients=ingredients)
+    
+@app.route("/new_ingredient")
+def new_ingredient():
+    return render_template("new_ingredient.html")
+
+@app.route("/add_ingredient", methods=["POST"])
+def add_ingredient():
+    name = request.form["name"]
+    sql = "INSERT INTO ingredient (name) VALUES (:name)"
+    db.session.execute(sql, {"name":name})
+    db.session.commit()
+    return redirect("/ingredients")
